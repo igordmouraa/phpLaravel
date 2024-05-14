62d56c0c0c8d6140dcafd8ff8477afabd6bc8608
igordmouraa <igomora04@gmail.com>
2024-05-14 13:11:31 -0300

    feat: Create CategoriasSeeder 'route'

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-        $this->call(CategoriasSeeder::class);
+        // $this->call(CategoriasSeeder::class);
+        // $this->call(UsersSeeder::class);
+        $this->call(ProdutosSeeder::class);
     }
 }
