2f795893e033388f22c76d49abe10ce782faea40
igordmouraa <igomora04@gmail.com>
2024-05-14 15:16:54 -0300

    feat: Create ProdutoController route

--- routes/web.php
+++ routes/web.php
@@ -3,4 +3,4 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProdutoController;
 
-Route::resource('/produtos', ProdutoController::class);
+Route::resource('/produtos', ProdutoController::class); 
