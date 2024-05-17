dc37d02d67dddd7e29b4bcec1b0b3984a0a5ff08
igordmouraa <igomora04@gmail.com>
2024-05-17 16:12:46 -0300

    feat: Create home page with products card and pagination redirect

--- resources/views/site/home.blade.php
+++ resources/views/site/home.blade.php
@@ -1,9 +1,29 @@
 @extends('site.layout')
-
 @section('title', 'BitStorm - Home Page')
-
 @section('conteudo')
 
-@if($nome == 'Igor')
-    true
-@endif
+<div class="row row-cols-1 row-cols-md-3 g-3 p-5">
+    @foreach($produtos as $produto)
+    <div class="col">
+        <div class="card h-80">
+            <img src="{{ $produto->imagem }}" class="card-img-top" alt="{{ $produto->nome }}">
+            <div class="card-body">
+                <h5 class="card-title">{{ $produto->nome }}</h5>
+                <p class="card-text">{{ $produto->descricao }}</p>
+                <a href="{{ route('site.details', $produto->slug)}}" class="btn btn-primary btn-sm">Veja agora!</a>
+            </div>
+            <div class="card-footer">
+                <small class="text-body-secondary">{{ $produto->updated_at }}</small>
+            </div>
+        </div>
+    </div>
+    @endforeach
+</div>
+
+<nav aria-label="Page navigation example">
+    <div class="d-flex justify-content-center">
+        {{ $produtos->links('custom.pagination') }}
+    </div>
+</nav>
+
+@endsection
